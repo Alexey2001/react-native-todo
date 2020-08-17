ae1db9afabf3411e1927de7d8ac65dc60a74ec29
alex <andr_2001alex_16@mail.ru>
2020-08-17 14:44:25 +0300

    adding window

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,15 +1,44 @@
 import React from 'react'
 import {StyleSheet, View, Text, Button} from 'react-native'
-
-export const TodoScreen = ({goBack, todo}) => {
+import {THEME} from '../theme'
+import { AppCard } from '../components/ui/AppCard'
+export const TodoScreen = ({goBack, todo, onRemove}) => {
     return (
-        <View>
-        <Text>{todo.title}</Text>
-        <Button title="Назад" onPress={goBack} />
+       <View>
+            <AppCard style={styles.card}>
+            <Text style={styles.title}>{todo.title}</Text>
+            <Button title='Ред.'/>
+            </AppCard>
+        <View style={styles.buttons}>
+        <View style={styles.button}>
+            <Button 
+                title="Назад" 
+                color={THEME.GRAY_COLOR}
+                onPress={goBack} />
         </View>
+        <View style={styles.button}>
+        <Button title="Удалить" 
+                color={THEME.DANGER_COLOR}
+                onPress={() => onRemove(todo.id)} />
+        </View>
+        </View>
+       </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    card: {
+        marginBottom: 20,
+        padding: 15
+    },
+    button: {
+        width: '40%'
+    },
+    title: {
+        fontSize: 20
+    }
 })
