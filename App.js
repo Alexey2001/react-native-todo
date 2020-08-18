064623d25dd45d22cad662eb1af868078dbfaf83
alex <andr_2001alex_16@mail.ru>
2020-08-18 23:33:37 +0300

    adding modal

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
-import {Navbar} from './src/components/Navbar'
-import { MainScreen } from './src/screens/MainScreen';
-import { TodoScreen } from './src/screens/TodoScreen';
-
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native'
+import { Navbar } from './src/components/Navbar'
+import { MainScreen } from './src/screens/MainScreen'
+import { TodoScreen } from './src/screens/TodoScreen'
 
 export default function App() {
-  const [todoId, setTodoId] = useState(null)
+  const [todoId, setTodoId] = useState('2')
   const [todos, setTodos] = useState([
-    {id: '1', title: 'Выучить react native'},
-    {id: '2', title: 'Выучить JS'}
+    { id: '1', title: 'Выучить React Native' },
+    { id: '2', title: 'Написать приложение' }
   ])
 
-  const addTodo = (title) => {
-    const newTodo = {
-      id: Date.now().toString(),
-      title: title
-    }
-
-    setTodos(prev => [...prev, {
-      id: Date.now().toString(),
-      title: title
-    }])
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
   }
 
   const removeTodo = id => {
@@ -34,43 +31,57 @@ export default function App() {
           text: 'Отмена',
           style: 'cancel'
         },
-        { text: 'Удалить',
+        {
+          text: 'Удалить',
           style: 'destructive',
-         onPress: () => {
-           setTodoId(null)
-           setTodos(prev => prev.filter(todo => todo.id !==id))
-        } }
+          onPress: () => {
+            setTodoId(null)
+            setTodos(prev => prev.filter(todo => todo.id !== id))
+          }
+        }
       ],
       { cancelable: false }
-    );
+    )
+  }
+
+  const updateTodo = (id, title) => {
+    setTodos(old =>
+      old.map(todo => {
+        if (todo.id === id) {
+          todo.title = title
+        }
+        return todo
+      })
+    )
   }
 
   let content = (
-    <MainScreen todos={todos} addTodo={addTodo} removeTodo={removeTodo}
+    <MainScreen
+      todos={todos}
+      addTodo={addTodo}
+      removeTodo={removeTodo}
       openTodo={setTodoId}
     />
   )
 
-  if(todoId) {
-    const selectedTodo = todos.find(todo => todo.id  === todoId)
-    content = <TodoScreen 
-    onRemove={removeTodo}
-     goBack={() => setTodoId(null)} todo={selectedTodo}/>
+  if (todoId) {
+    const selectedTodo = todos.find(todo => todo.id === todoId)
+    content = (
+      <TodoScreen
+        onRemove={removeTodo}
+        goBack={() => setTodoId(null)}
+        todo={selectedTodo}
+        onSave={updateTodo}
+      />
+    )
   }
 
   return (
     <View>
-      <Navbar title="Todo App!"/>
-      <View style={styles.container}>
-      { content }
-      {/* <View>
-        { todos.map(todo => {
-          return <Todo todo={todo} key={todo.id}/>
-        }) }
-      </View> */}
-      </View>
+      <Navbar title='Todo App!' />
+      <View style={styles.container}>{content}</View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -78,4 +89,4 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     paddingVertical: 20
   }
-});
+})
--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -3,31 +3,44 @@ import {StyleSheet, View, Text, Button} from 'react-native'
 import {THEME} from '../theme'
 import { AppCard } from '../components/ui/AppCard'
 import { EditModal } from '../components/EditModal'
-export const TodoScreen = ({goBack, todo, onRemove}) => {
+
+export const TodoScreen = ({goBack, todo, onRemove, onSave}) => {
     const [modal, setModal] = useState(false)
+
+    const  saveHandler = (title) => {
+        onSave(todo.id, title)
+        setModal(false)
+    }
+
     return (
-       <View>
-        <EditModal visible={modal} onCancel={() => setModal(false)}/>
-            <AppCard style={styles.card}>
+        <View>
+          <EditModal
+            value={todo.title}
+            visible={modal}
+            onCancel={() => setModal(false)}
+            onSave={saveHandler}
+          />
+    
+          <AppCard style={styles.card}>
             <Text style={styles.title}>{todo.title}</Text>
-            <Button title='Ред.' onPress={() => setModal(true)}/>
-            </AppCard>
-        <View style={styles.buttons}>
-        <View style={styles.button}>
-            <Button 
-                title="Назад" 
-                color={THEME.GRAY_COLOR}
-                onPress={goBack} />
-        </View>
-        <View style={styles.button}>
-        <Button title="Удалить" 
+            <Button title='Ред.' onPress={() => setModal(true)} />
+          </AppCard>
+    
+          <View style={styles.buttons}>
+            <View style={styles.button}>
+              <Button title='Назад' onPress={goBack} color={THEME.GREY_COLOR} />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title='Удалить'
                 color={THEME.DANGER_COLOR}
-                onPress={() => onRemove(todo.id)} />
+                onPress={() => onRemove(todo.id)}
+              />
+            </View>
+          </View>
         </View>
-        </View>
-       </View>
-    )
-}
+      )
+    }
 
 const styles = StyleSheet.create({
     buttons: {
