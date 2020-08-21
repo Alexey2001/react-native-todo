5c16afed013cba335780aa7d9917ccd12aa207fd
alex <andr_2001alex_16@mail.ru>
2020-08-21 23:57:08 +0300

    adding pic

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,24 +1,53 @@
 import React from 'react'
-import {StyleSheet, View, FlatList} from 'react-native'
+import {StyleSheet, View, FlatList, Text, Image} from 'react-native'
 import {AddTodo} from '../components/AddTodo'
 import {Todo} from '../components/Todo'
 export const MainScreen = ({addTodo, todos, removeTodo, openTodo}) => {
+    let content = (
+      <FlatList
+      keyExtractor={item => item.id.toString()}
+      data={todos}
+      renderItem={({item}) => {
+        return <Todo todo={item} onRemove={removeTodo}
+          onOpen={openTodo}
+        />
+      }}
+    />
+    )
+
+    if(todos.length === 0) {
+      content =(
+         <View style={styles.imgWrap}>
+        <Image style={styles.image} source={require('../../assets/no-items.png.png')}/>
+        {/* <Image 
+        style={styles.image} 
+        source={{
+          uri: 'http://samuraigeeks.net/wp-content/uploads/2019/06/373-3738691_react-native-svg-transformer-allows-you-import-svg.jpg'
+        }}/> */}
+
+      </View>
+
+      )
+    }
+
     return (
      <View>
         <AddTodo onSubmit={addTodo}/>
-      <FlatList
-        keyExtractor={item => item.id.toString()}
-        data={todos}
-        renderItem={({item}) => {
-          return <Todo todo={item} onRemove={removeTodo}
-            onOpen={openTodo}
-          />
-        }}
-      />
+        {content}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+  imgWrap: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    height: 300
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain'
+  }
 })
